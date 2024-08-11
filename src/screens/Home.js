0b927a98e84246fdcc05ad50d5a382f2de4dc1a0
Aditya Shashi <adityatulip2@gmail.com>
2024-08-12 04:40:14 +0530

    Implimented Card, footer and carousel

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,35 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Card from '../components/Card'
+import Carousel from '../components/Carousel'
 
 export default function Home() {
     return (
         <div>
             <div><Navbar /></div>
-            <div>
-                <div className="card mt-3" style={{ "width": "18rem", "maxHeight":"360px" }}>
-                    <img src="..." className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is some important text.</p>
-                        <div className="container w-100">
-                            <select className="m-2 h-100 bg-success rounded">
-                                {Array.from(Array(6), (e,i)=>{
-                                    return(
-                                        <option key={i+1} value={i+1}> {i+1} </option>
-                                    )
-                                })}
-                            </select>
-                            <select className="h-100 m-2 bg-success rounded">
-                                <option value="half">Half</option>
-                                <option value="Full">Full</option>
-                            </select>
-                            <div className="d-inline h-100 fs-5">
-                                Total Price
-                            </div>
-                        </div>
-                    </div>
-                </div></div>
+            <div><Carousel/></div>
+            <div> <Card/> </div>
             <div><Footer /></div>
 
         </div>
